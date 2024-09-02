26cdfa6ee1daa553aaae4f71c28f3dba74ea279c
harishraajaa <rvharishraajaa@gmail.com>
2024-09-02 12:50:31 +0530

    Login page Done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
-
+import React,{useEffect} from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+//import api from '../service/apiService'
+//import ApiRoutes from '../utils/ApiRoutes';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+//import toast from 'react-hot-toast';
 function Login() {
+
+  const navigate = useNavigate()
+
+
   return (
-    <div>Login</div>
+    <div className='login-wrapper'>
+
+      <h3 className='text-align-center'>Welcome to the world of recipes!</h3>
+
+      <p className='text-align-center'>Don't have an accout? SignUp <Link to='/signup'>Here</Link></p>
+
+      <Form>
+      <Form.Group className="mb-3">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" name='email'/>
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" name='password'/>
+      </Form.Group>
+
+      <div className='text-align-center'>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+      </div>
+    </Form>
+    </div>
   )
 }
 
